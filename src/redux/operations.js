dac22ce6753a34d24ec1e911f5756397f1a7efaf
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2022-12-20 13:55:46 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,5 +1,4 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import customNotiflix from 'utils/notiflix';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://63a05fd5e3113e5a5c3bbb27.mockapi.io';
@@ -9,10 +8,8 @@ export const fetchContacts = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const response = await axios.get('/contacts');
-      console.log(response.data);
       return response.data;
     } catch (error) {
-      console.log(error.message);
       return thunkAPI.rejectWithValue(error.message);
     }
   }
